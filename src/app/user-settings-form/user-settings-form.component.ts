62b8b4e530493fa36ed5ec4f7703833185522649
Kapil Ghimire <kapilghimire.ghimire@gmail.com>
2022-07-31 07:12:50 +0545

    implemented template and ngModel two way binding

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { UserSettings } from "../data/user-settings";
 
 @Component({
   selector: 'user-settings-form',
@@ -6,5 +7,15 @@ import { Component } from "@angular/core";
   styles: []
 })
 export class UserSettingsFormComponent {
+
+  userSettings:UserSettings={
+    name:'Miltion',
+    emailOffers:true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes:'here are some notes'
+  }
+
+
 
 }
